9746008916abdba0a596301dc3d0f22dcc258772
UserBug0 <lyiozel@gmail.com>
2022-07-15 14:47:38 +0300

    Set up Project

--- RayTracing/src/RayTracing.cpp
+++ RayTracing/src/RayTracing.cpp
@@ -3,10 +3,48 @@
 
 #include "RayTracing.h"
 
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/vec3.hpp>
+
+#include <ranges>
+
+#include <ImGui/imgui.cpp>
+
+
 using namespace std;
 
 int main()
 {
+	glm::vec3 v(1);
 	cout << "Hello CMake." << endl;
+
+	if (glfwInit())
+	{
+		std::cout << "GLFW Succesfully initialized\n";
+	}
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "RayTracing", NULL, NULL);
+	if (window == NULL)
+	{
+		cout << "Failed to create GLFW window" << endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+	if (status)
+	{
+		std::cout << "glad Successfully initialized\n";
+	}
+
+	while (!glfwWindowShouldClose(window))
+	{
+		continue;
+	}
+	glfwTerminate();
+
 	return 0;
 }
