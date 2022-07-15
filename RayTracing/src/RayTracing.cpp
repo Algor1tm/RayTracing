ddfef1ce5f2ee380814e0693fb20583f2c5c0572
UserBug0 <lyiozel@gmail.com>
2022-07-15 13:15:13 +0300

    Add project files.

---
+++ RayTracing/src/RayTracing.cpp
@@ -0,0 +1,12 @@
+﻿// RayTracing.cpp : Defines the entry point for the application.
+//
+
+#include "RayTracing.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ RayTracing/src/RayTracing.h
@@ -0,0 +1,8 @@
+﻿// RayTracing.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
